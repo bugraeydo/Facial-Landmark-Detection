ac5f694049aaf819ff8a26d46c8aa5e45a862544
unknown <bugra.eyidogan@gmail.com>
2023-06-19 01:23:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+//Install Dependencies DONE
+//Import Dependencies DONE
+//Setup webcam and canvas DONE
+//Define references to those DONE
+//Load facemesh
+//Detect function DONE
+//Detect function DONE
+//Drawing utilities from tensorflow DONE
+//Draw functions DONE
+
+import React, {useRef} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from 'react-webcam';
+import { drawMesh } from './utilities';
 
 function App() {
+  //Setup references
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  //Loadfacemesh
+  const runFacemesh = async () => {
+    // OLD MODEL
+    const net = await facemesh.load({
+       inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+     });
+     setInterval(() => {
+      detect(net);
+    }, 100);
+    };
+
+    const detect = async (net) => {
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+      ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      // OLD MODEL
+             const face = await net.estimateFaces(video);
+      // NEW MODEL
+      //const face = await net.estimateFaces({input:video});
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+      }
+    };
+    runFacemesh();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <header className="App-header">
+      <Webcam ref={webcamRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+      }} />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+          </header>
     </div>
   );
 }
